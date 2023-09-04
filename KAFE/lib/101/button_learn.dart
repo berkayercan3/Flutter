bd39e69de6e85305fa82c206ee0555717d815ab2
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-09-04 21:01:12 +0300

    Update button_learn.dart
    Button creation and editing is finished

--- KAFE/lib/101/button_learn.dart
+++ KAFE/lib/101/button_learn.dart
@@ -9,20 +9,61 @@ class ButtonLearn extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children:  [
+        children: [
           TextButton(
-              child: const Text('Button'),
+            child: Text('Button', style: Theme.of(context).textTheme.subtitle1),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.green;
+              }
+              return Colors.red;
+            })),
             onPressed: () {}, // düğmeye tıklanınca yapılacak işlemler
           ),
-          ElevatedButton(onPressed: () {}, child: const Text('data')),
-          IconButton(onPressed: ()  {}, icon: const Icon(Icons.abc_rounded)),
-          FloatingActionButton(onPressed: () {},
-          child: const Icon(Icons.add),),
-          OutlinedButton(onPressed: () {}, child: const Text('data')),
-          InkWell( // daha özelleştirilebilir bir button yaratmamıza olanak tanır
+          const ElevatedButton(onPressed: null, child: const Text('data')),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.abc_rounded)),
+          FloatingActionButton(
+            onPressed: () {
+              //servise istek at
+              //sayfa rengini düzenle
+            },
+            child: const Icon(Icons.add),
+          ),
+          OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  //shape: const CircleBorder() daire şeklini yaratır
+                  backgroundColor: Colors.red,
+                  padding: const EdgeInsets.all(10)),
+              onPressed: () {},
+              child: const SizedBox(width: 200, child: Text('abcdef'))),
+          InkWell(
+            // daha özelleştirilebilir bir button yaratmamıza olanak tanır
             onTap: () {},
-            child:const Text('özelleştirilebilir'),
-          )
+            child: const Text('özelleştirilebilirButton'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
+              minimumSize: Size(300, 100),
+            ),
+            onPressed: () {},
+            child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 20, bottom: 20, right: 40, left: 40),
+                child: Text(
+                  'Success',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                )),
+          ),
+          // const SizedBox(height: 10),
         ],
       ),
     );
