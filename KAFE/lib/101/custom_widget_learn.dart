9d427742b47dde300b8e2b541faca0b9d9cf4094
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-09-14 02:08:36 +0300

    CustomWidget_LearnTwo

--- KAFE/lib/101/custom_widget_learn.dart
+++ KAFE/lib/101/custom_widget_learn.dart
@@ -3,16 +3,28 @@ import 'package:flutter/material.dart';
 class CustomWidgetLearn extends StatelessWidget {
   const CustomWidgetLearn({super.key});
 
+  final String title = 'Food';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Column( //column ile butonun ekrana yatay sığdırdık
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // hizalama yapmak icin kullandik
+        //column ile butonun ekrana yatay sığdırdık
         children: [
-          Center(child: Padding(
-            padding:  const EdgeInsets.symmetric(horizontal: 10),
-            child:SizedBox(width: MediaQuery.of(context).size.width ,child: CustomButton()),
+          Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SizedBox(
+                   width: MediaQuery.of(context).size.width, // ekran genisligi ne ise o boyuta sahip bir buton oluyor
+                  child: CustomButton(title: title,onPressed: () {})
+                ),
           )),
+          const SizedBox(height: 100),
+          Center(child:
+            CustomButton(title: title,onPressed: () {})
+          ),
         ],
       ),
     );
@@ -25,30 +37,27 @@ class ButtonColor {
 }
 
 class PaddingUtility {
-
-final EdgeInsets normalPadding  = const EdgeInsets.all(15);
+  final EdgeInsets normalPadding = const EdgeInsets.all(15);
+  final EdgeInsets rightPadding = const EdgeInsets.symmetric(horizontal: 20);
 }
 
-
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-  final String tittle = 'Food';
+  const CustomButton({Key? key, required this.title, required this.onPressed}) : super(key: key);
+  final String title;
+  final void Function() onPressed; // butonun hangi işi yapacağını belirttik
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-          primary: ButtonColor().redColor, shape: StadiumBorder()),
-      onPressed: () {},
-      child: Center(
-        child: Padding(
+          primary: ButtonColor().redColor, shape: const StadiumBorder()),
+      onPressed: onPressed,
+      child: Padding(
           padding: PaddingUtility().normalPadding,
           child: Text(
-            tittle,
+            title,
             style: Theme.of(context).textTheme.subtitle2?.copyWith(
                 color: ButtonColor().white, fontWeight: FontWeight.bold),
-          )
-        ),
-      ),
+          )),
     );
   }
 }
